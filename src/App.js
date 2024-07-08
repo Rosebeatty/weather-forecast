04223eb0d211da3b320a1b4659e2f9f04bb26f60
Rose Beatty <rose.beatty@gmail.com>
2024-07-08 10:40:03 +0200

    feat(routes): create routes and styles for app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Location from "./pages/Location";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  background: linear-gradient(to left, #ffafbd, #ffc3a0);
+  height: 100%;
+  min-height: 100vh;
+  margin: 0 auto;
+  padding: 0.6em 1.4em;
+  text-align: center;
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route exact path="/weather/:location" element={<Location />} />
+        </Routes>
+      </BrowserRouter>
+    </StyledApp>
   );
 }
 
